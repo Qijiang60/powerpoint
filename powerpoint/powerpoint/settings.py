5db58abab1aca9d81eb85ebfff59ea111b7e9c8d
shengfu.chen <823089260@qq.com>
2015-07-29 18:38:47 +0800

    test

--- powerpoint/powerpoint/settings.py
+++ powerpoint/powerpoint/settings.py
@@ -87,3 +87,75 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING_FILE = 'log.log'
+
+LOGGING = {
+        'version': 1,
+            'disable_existing_loggers': True,
+                'formatters': {
+                            'standard': {
+                                            'format': '%(asctime)s [%(threadName)s:%(thread)d] [%(name)s:%(lineno)d] [%(levelname)s]- %(message)s'
+                                                        },
+                                },
+                    'filters': {
+                            },
+                        'handlers': {
+                                    'mail_admins': {
+                                                    'level': 'ERROR',
+                                                                'class': 'django.utils.log.AdminEmailHandler',
+                                                                            'include_html': True,
+                                                                                    },
+                                            'default': {
+                                                            'level':'DEBUG',
+                                                                        'class':'logging.handlers.RotatingFileHandler',
+                                                                                    'filename': 'all.log',
+                                                                                                'maxBytes': 1024*1024*5,
+                                                                                                            'backupCount': 5,
+                                                                                                                        'formatter':'standard',
+                                                                                                                                },
+                                                    'console':{
+                                                                    'level': 'DEBUG',
+                                                                                'class': 'logging.StreamHandler',
+                                                                                            'formatter': 'standard'
+                                                                                                        },
+                                                            'request_handler': {
+                                                                            'level':'DEBUG',
+                                                                                        'class':'logging.handlers.RotatingFileHandler',
+                                                                                                    'filename': 'all.log', 
+                                                                                                                'maxBytes': 1024*1024*5,
+                                                                                                                            'backupCount': 5,
+                                                                                                                                        'formatter':'standard',
+                                                                                                                                                },
+                                                                    'scprits_handler': {
+                                                                                    'level':'DEBUG',
+                                                                                                'class':'logging.handlers.RotatingFileHandler',
+                                                                                                            'filename': 'all.log', 
+                                                                                                                        'maxBytes': 1024*1024*5,    
+                                                                                                                                    'backupCount': 5,
+                                                                                                                                                'formatter':'standard',
+                                                                                                                                                        },
+                                                                        },
+                            'loggers': {
+                                        'django': {
+                                               'handlers': ['default','console'],
+                                                                    'level': 'DEBUG',
+                                                                                'propagate': False
+                                                                                            },
+                                                'powerpoint':{
+                                                                'handlers': ['default','console'],
+                                                                            'level': 'DEBUG',
+                                                                                        'propagate': True         
+                                                                                                    },
+                                                        'django.request': {
+                                                                        'handlers': ['request_handler'],
+                                                                                    'level': 'DEBUG',
+                                                                                                'propagate': False
+                                                                                                            },
+                                                                'scripts': {
+                                                                                'handlers': ['scprits_handler'],
+                                                                                            'level': 'INFO',
+                                                                                                        'propagate': False
+                                                                                                                    },
+                                                                    }
+}          
--- powerpoint/ppt/views.py
+++ powerpoint/ppt/views.py
@@ -1,15 +1,26 @@
+import logging
 from django.shortcuts import render_to_response
 from django.conf.urls import url,patterns
 from django.views.generic import View
+from django.http import HttpResponseNotFound
 from ppt.models import PPTWorkData
 
 class IndexAction(View):
     template_name = 'ppt.html'
 
     def get(self, request,token):
+        logger = logging.getLogger("powerpoint")
+        logger.info(token)
+        context = {}
         infos = PPTWorkData.objects.filter(token=token).values("title")
-
-        return render_to_response(self.template_name,infos)    
+        if len(infos) == 0:
+            return HttpResponseNotFound()
+        else:
+            info = infos[0]
+        context.update(info)
+        logger.info("infos")
+        logger.info(infos)
+        return render_to_response(self.template_name,context)    
 
 urlpatterns = patterns(
     '',

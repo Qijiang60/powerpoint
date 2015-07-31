44bebfb9bd4f5b88eaa7abc5284e983be98936e2
shengfu.chen <823089260@qq.com>
2015-07-31 12:55:51 +0800

    选择模板

--- powerpoint/ppt/admin.py
+++ powerpoint/ppt/admin.py
@@ -1,5 +1,6 @@
 #coding:utf-8
-
+import logging
+import string
 from django.contrib import admin
 from ppt.models import PowerPoint, Page
 from ppt.tools import writeJsonp 
@@ -13,11 +14,32 @@ class PowerPointAdmin(admin.ModelAdmin):
    inlines = [PageInline]  
 
    def save_model(self,  request, obj, form, change):
-       page_dict = { "code": 200, "data": {"thumbnail":None,"description":"每一页,都美若人生初见.","mode":"push","pageSwitch":{"animateId":"default"},"backgroud":{"color":None},"music":{"src":None},"pages":[{"layout":{"name":None,"label":"SingleImage","image":[{"url":"http://img01.cloud7.com.cn/371a9a98071ed29cbd09f3f8d4102e38.jpg","imageinfo":{"x":0,"y":0,"width":320,"height":504,"maskRadius":0.0,"type":"image","borderWidth":0}}],"custom":{"type":"y","animationFirst":"image"}}}],"author":"Mr. Chen","headimgurl":"http://wx.qlogo.cn/mmopen/iajwTFkyW3R5gpRtAia2TkQpVKcBTWFjxjWqP06FIsTILVdzqNECHUQ52lDKevlJxe2nTiaGzaf40HCsFvLibuWtrqAXXnDib8SpA/0","copyright":1,"praise":0,"uid":1499404} }
+       logger = logging.getLogger("powerpoint")
+      
+       page_dict = {"code":200,"data":{"thumbnail":None,"description":"每一页,都美若人生初见.","mode":"push","pageSwitch":{"animateId":"default"},"backgroud":{"color":None},"music":{"src":None},"author":"Mr. Chen","headimgurl":"http://wx.qlogo.cn/mmopen/iajwTFkyW3R5gpRtAia2TkQpVKcBTWFjxjWqP06FIsTILVdzqNECHUQ52lDKevlJxe2nTiaGzaf40HCsFvLibuWtrqAXXnDib8SpA/0","userworks":None,"copyright":1,"praise":0,"theme":None,"uid":1499404}}
+       pages = []
+       i = 0
+       total = string.atoi(request.POST['page_set-TOTAL_FORMS'])
+       logger.info("total:") 
+       logger.info(total)
+       while i < total: 
+           page = {"layout":{"name":None,"label":"MutipleImage01","image":[],"text":None,"voice":None,"video":None,"location":None,"actionlinks":None,"position":None,"custom":{"type":"y","animationFirst":"image","performace":None},"signup":None}}
+           page['layout']['label'] = request.POST['page_set-' + str(i) + '-label']
+           j = 0
+           while j < 9:
+               if request.POST['page_set-' + str(i) + '-img' + str(j + 1)]:
+                   image = {"imageinfo":{"x":0,"y":0,"width":320,"height":504,"maskRadius":0.0,"type":"image","borderWidth":0}}
+                   image['url'] = request.POST['page_set-' + str(i) + '-img' + str(j + 1)]
+                   page['layout']['image'].insert(j, image)
+               j += 1
+               logger.info("j:")
+               logger.info(j)
+           pages.insert(i, page)
+           i += 1
+
        page_dict['data']['title'] = request.POST['title']
-       obj.token = request.POST['token']
-       obj.title = request.POST['title']
-       writeJsonp('%s' % obj.token, page_dict)
-       obj.save() 
+       page_dict['data']['pages'] = pages
+       writeJsonp('%s' % request.POST['token'], page_dict)
+       obj.save()
 
 admin.site.register(PowerPoint, PowerPointAdmin)
